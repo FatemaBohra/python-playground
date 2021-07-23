9a8049005de0f1db0a35b6517d369ce6c98c8ec3
Fatema Bohra <fatema.bohra2003@gmail.com>
2021-07-23 11:55:23 +0530

    Searches a dict

---
+++ src/search_by_key_in_dictionary.py
@@ -0,0 +1,5 @@
+def search_by_key_in_dictionary(dictionary, search_string):
+    return dictionary[search_string]
+
+
+print(search_by_key_in_dictionary({"name": "Fatema", "age": 18, "course": "Computer Science"}, "name"))
