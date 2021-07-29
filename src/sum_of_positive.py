b3fa541762978872cf2a0abd094dfe133354109b
Fatema Bohra <fatema.bohra2003@gmail.com>
2021-07-29 08:34:48 +0530

    Sums all the positive numbers

---
+++ src/sum_of_positive.py
@@ -0,0 +1,12 @@
+# You get an array of numbers, return the sum of all
+# of the positives ones.
+
+def positive_sum(arr):
+    sum = 0
+    for i in arr:
+        if i > 0:
+            sum += i
+    return sum
+
+
+print(positive_sum([-1,2,3,4,-5]))
