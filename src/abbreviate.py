b4e11348803ec85b34ba21806cc99587863fb812
Fatema Bohra <fatema.bohra2003@gmail.com>
2021-07-31 03:54:56 +0530

    Abbreviates the name

---
+++ src/abbreviate.py
@@ -0,0 +1,10 @@
+# Write a function to convert a name into initials. This kata strictly
+# takes two words with one space in between them.
+# The output should be two capital letters with a dot separating them.
+
+def abbrev_name(name):
+    name_list = name.split(" ")
+    return name_list[0][0].upper() + "." + name_list[1][0].upper()
+
+
+print(abbrev_name("Sam Harris"))
